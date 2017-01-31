bfc410e42ec5a1774b9cb51325d5d3b077324aa8
Aybike Aydemir <aybikeaydemir.94@gmail.com>
2017-01-31 15:05:29 +0300

    MY HOMEWORK1's last version

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 Elif Aybike Aydemir 
 Bil 481 HW1 
-26.01.2017 
+141101074
+31.01.2017 
+
+This program ;
+
+In mathematics, a binary relation on a set A is a collection elements of A. In other words, it is a subset of the Cartesian product A2 = A × A. This application checks A's binary combinations.<br>
+  In the first form,enter a list of integers separeted by a newline character.<br>
+  In the second form,enter two integer.(We have two boxes for this )<br>
+  If this two integers' combination are found in the list,it is a binary combinations,the result becomes true.<br><br>
 
 
-[![Build Status](https://travis-ci.org/aybikeaydemir/myDemoApp.svg?branch=master)](https://travis-ci.org/aybikeaydemir/myDemoApp)
+
 
 
 Demo site: https://stormy-woodland-33416.herokuapp.com/
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,33 +4,22 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import java.util.Arrays;
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+    
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    
     public void testApp()
     {
         assertTrue( true );
@@ -54,8 +43,9 @@ public class AppTest
       assertFalse(new App().binaryRelation(null, 1,6));
     }
 
+   
 
-
+     
 
 
 }
